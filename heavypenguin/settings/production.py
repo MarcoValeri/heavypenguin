1372f2dead96297dbedb6cc871851347660d04c2
Marco Valeri <marcovaleri@hotmail.it>
2021-11-22 12:52:36 +0000

    Set up sqlite db

--- heavypenguin/settings/production.py
+++ heavypenguin/settings/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['devmarcovaleri.ue.eu.pythonanywhere.com']
+
+ROOT_URLCONF = 'heavypenguin.urls'
+
+SECRET_KEY = 'u%k+9u+(scbg!0rdq!f0ejdcyh+ap#7(j3e-b13fepp+2(n_8n'
+
 try:
     from .local import *
 except ImportError:
